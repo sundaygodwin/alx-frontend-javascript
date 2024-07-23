9cb14feb6efe268e044a99bce74c6cc0fb00f78d
“sundaygodwin” <godwinsunday194@gmail.com>
2024-07-23 21:26:35 +0000

    correction of task

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-// Define and export the handleProfileSignup function
-export async function handleProfileSignup(firstName, lastName, fileName) {
-  // Call the two functions and store their promises
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-  
-  // Wait for all promises to settle
-  const results = await Promise.allSettled([signUpPromise, uploadPromise]);
-
-  // Return an array with the status and value/error of each promise
-  return results.map(result => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason
-  }));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => 
+    results.map(result => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason
+    }))
+  );
 }
